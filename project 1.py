7e4de176caccbb87dbfdeeac7e59452fe5f300ca
sairithvik123 <sairithviks2011@gmail.com>
2024-07-21 19:35:05 +0530

    Add files via upload

---
+++ project 1.py
@@ -0,0 +1,10 @@
+#Traffic light simulator
+color=str(input("enter the color"))
+if(color=="red"):
+    print("stop")
+elif(color=="yellow"):
+    print("get redy")
+elif (color=="green"):
+    print("go")
+else:
+    print("type a valid color")
